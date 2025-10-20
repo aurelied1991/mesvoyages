3fb0e38a8961ec98e45b68320cd896e2e5cfeff1
aurelied1991 <aurelie.demange1991@gmail.com>
2025-10-20 22:34:07 +0200

    Phase 24 : déploiement

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -51,7 +51,7 @@ class ContactController extends AbstractController {
     public function sendEmail(MailerInterface $mailer, Contact $contact) {
         $email = (new Email())
                 ->from($contact->getEmail())
-                ->to('contact@mesvoyages.com')
+                ->to('aureliedemangetestdeploiement@gmail.com')
                 ->subject('Message du site de voyages')
                 ->html($this->renderView(
                         'pages/_email.html.twig', [
